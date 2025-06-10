74a0e559a08f47b95a4d328612b0234cf841d672
vl05953 <adam.szilagyi@ksh.hu>
2025-06-10 08:20:31 +0200

    Adding ROracle package too

--- Main.R
+++ Main.R
@@ -1,3 +1,16 @@
+# install.packages("ROracle.zip", repos = NULL)
+library("ROracle")
+drv <- Oracle()
+con <- dbConnect(drv, username = Sys.getenv("userid"), password = Sys.getenv("pwd"), dbname = "emerald.ksh.hu")
+
+# res <- dbSendQuery(con, "select * from VB_REP.VB_APP_INIT where ALKALMAZAS like 'MNB napi változáslista küldése' and PROGRAM = 'mnb_EBEAD.sql' and PARAM_NEV = 'utolso_futas'")
+# (data <- fetch(res))
+# str(data)
+# 
+# res2 <- dbSendQuery(con, "select distinct AAJE, upper(VNEVEM) VNEVEM, upper(UNEVEM) UNEVEM, upper(SZVNEVE) SZVNEVE, upper(SZUNEVE) SZUNEVE, upper(AVENVE) AVNEVE, upper(AUNEVE) AUNEVE, to_char(lg.lg_naptar_uj.szulido_adoazbol(AAJE), 'YYYY-MM-DD') SZUL_DAT from LG23.VNAA0_2326_230112_V00 where (upper(VNEVEM) like 'A%' or upper(VNEVEM) like 'Á%') order by upper(VNEVEM), upper(UNEVEM)")
+# (data2 <- fetch(res2))
+# str(data2)
+
 library("lubridate")
 worksheet_firsthalf <- read.csv(paste0(getwd(), "/Excel_Files/969_KSH_attrakcio_minta_pinot_kozfurdok_20240415_0430.csv"), header = TRUE, sep = ";", colClasses = "character")
 
@@ -14,3 +27,15 @@ worksheet_firsthalf$program_vege <- ymd_hm(gsub(". ", "-", worksheet_firsthalf$p
 
 Sys.Date()
 Sys.time()
+
+Sys.getenv("TZ") ""
+Sys.getenv("ORA_SDTZ") ""
+
+Sys.setenv(TZ = "GMT")
+Sys.setenv(ORA_SDTZ = "GMT")
+
+rs3 <- dbSendQuery(con, "insert into GA.DATUM(idopont) values (:1)", data = worksheet_firsthalf$idopont)
+execute(rs3, data = worksheet_firsthalf$idopont)
+dbCommit(con)
+
+dbDisconnect(con)
